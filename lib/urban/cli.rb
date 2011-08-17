f07d33f44228e7976e74affca2f1f58135a7a86c
Tom Miller <tom.miller@luckie.com>
2011-08-17 07:30:22 -0500

    Adding support to look up a phrase

--- lib/urban/cli.rb
+++ lib/urban/cli.rb
@@ -4,10 +4,10 @@ module Urban
 
   def process
     begin
-      result = ARGV.first ? define(ARGV.first) : random
+      result = ARGV.first ? define(ARGV.join(' ')) : random
       puts "#{result[:word]}: #{result[:definition]}"
     rescue
-      puts "ERROR: #{ARGV.first.capitalize} not found!"
+      puts "ERROR: #{ARGV.join(' ')} not found!"
     end
   end
 
