7a9d2e13e1ba2338f30b8dda449bd105a9a39b82
Tom Miller <jackerran@gmail.com>
2011-09-05 20:17:48 -0500

    Remove uneeded test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,23 +6,13 @@ require 'minitest/autorun'
 require 'urban'
 require 'urban/cli'
 
-TEST_ENTRY = Urban::Dictionary::Entry.new(
-  'impromptu',
+TEST_ENTRY = Urban::Dictionary::Entry.new( 'impromptu',
   [ 'Something that is made up on the spot and given little time to gather and present. Usually referring to speeches that are given only a few minutes to prepare for.',
     'On the spot',
     'Something that is made up on the spot.  Can also mean a speech that was made with little or no preparation.' ])
 
 def load_file(filename)
   IO.read(File.expand_path("../data/#{filename}", __FILE__))
-end
-
-['refute', 'assert'].each do |action|
-  eval <<-EOM
-    def #{action}_cli_prints(matches, &block)
-      out, err = capture_io(&block)
-      [*matches].each { |match| #{action}_match(match, out) }
-    end
-  EOM
 end
 
 module Stub
